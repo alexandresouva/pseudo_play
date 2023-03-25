4312e62442f5a63073120539833cc21e9c137912
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-03-25 00:09:51 -0300

    feat: add function to list videos (GET method)

---
+++ js/connectApi.js
@@ -0,0 +1,29 @@
+'use strict';
+
+async function listVideos() {
+  const response = await fetch('http://localhost:5000/videos');
+  const data = await response.json();
+  return data;
+}
+
+async function createVideo(title, views, videoUrl, imageSrc) {
+  const connection = await fetch('http://localhost:5000/videos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      titulo: title,
+      visualizacoes: `${views} mil visualizações`,
+      url: videoUrl,
+      imagem: imageSrc,
+    }),
+  });
+  const data = await connection.json();
+  return data;
+}
+
+export const connectApi = {
+  listVideos,
+  createVideo,
+};
---
+++ js/showVideos.js
@@ -0,0 +1,31 @@
+'use strict';
+import { connectApi } from './connectApi.js';
+
+const $list = document.querySelector('[data-list]');
+
+function createCard(title, views, videoSrc, imgSrc) {
+  const card = document.createElement('li');
+  card.classList.add('videos__item');
+  card.innerHTML = `<iframe width="100%" height="72%" src="${videoSrc}"
+  title="${title}" frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen></iframe>
+<div class="descricao-video">
+  <img src="${imgSrc}">
+  <h3>${title}</h3>
+  <p>${views}</p>
+</div>`;
+
+  return card;
+}
+
+async function listVideo() {
+  const list = await connectApi.listVideos();
+  list.forEach((item) => {
+    $list.appendChild(
+      createCard(item.titulo, item.visualizacoes, item.url, item.imagem)
+    );
+  });
+}
+listVideo();
+// console.log(connectApi);
